fa667f184a3d83ab0088af5e488c951c9d298dfc
Dave <dave_sum69@hotmail.com>
2026-03-14 17:53:03 +0530

    Changes

--- app.js
+++ app.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const myelement = React.createElement(
-    "h1",
-    {id: "myid", className: "myclass",key:"mykey"},
-    "Hello World!"
-)
-const myelement1 = React.createElement(
-    "h2",
-    {id: "myid1", className: "myclass",key:"mykey1"},
-    "Hello World!"
-)
-const divcont = React.createElement(
-    "div",
-    {
-        id: "myid",
-        className: "myclass"
-    },
-    [myelement,myelement1]
-)
+const mainmenu = React.createElement(
+  "nav",
+  {
+    id: "mymenu",
+    className: "navbar",
+  },
+  [
+    React.createElement(
+      "ul",
+      { id: "mygroup1", className: "nav-list", key: "choicegroup1" },
+      [
+        React.createElement(
+          "li",
+          { id: "mychoice1", className: "nav-item", key: "choice1" },
+          [
+            React.createElement(
+              "a",
+              {
+                id: "mychoice1",
+                href: "#home",
+                className: "nav-link",
+                key: "choice1",
+              },
+              "Choice 1",
+            ),
+          ],
+        ),
+        React.createElement(
+          "li",
+          { id: "mychoice2", className: "nav-item", key: "choice2" },
+          [
+            React.createElement(
+              "a",
+              {
+                id: "mychoice2",
+                href: "#home",
+                className: "nav-link",
+                key: "choice2",
+              },
+              "Choice 2",
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(divcont);
+const BreakingNews = () => {
+  return (
+    <div className="newcontainer">
+      <h3>Breaking News</h3>
+      <ul>
+        <li>News 1</li>
+        <li>News 2</li>
+        <li>News 3</li>
+      </ul>
+    </div>
+  );
+};
+
+const DisplayComponent = () => {
+  return (
+    <h3>
+      Display Functional Component
+      {mainmenu}
+      <BreakingNews/>
+    </h3>
+
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<DisplayComponent />);
